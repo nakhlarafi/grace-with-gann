748468de72d9fc9f13a395e48182adee949e0d44
nakhlarafi <nakhla054@gmail.com>
2023-08-04 15:22:56 -0400

    chang in the lang dim to 350x350

--- run.py
+++ run.py
@@ -20,8 +20,8 @@ class dotdict(dict):
     def __getattr__(self, name):
         return self[name]
 
-NlLen_map = {"Time":3900, "Math":4500, "Lang":280, "Chart": 1000, "Mockito":650, "unknown":2200}
-CodeLen_map = {"Time":1300, "Math":2700, "Lang":300, "Chart":5250, "Mockito":1176, "unknown":2800}
+NlLen_map = {"Time":3900, "Math":4500, "Lang":350, "Chart": 1000, "Mockito":650, "unknown":2200}
+CodeLen_map = {"Time":1300, "Math":2700, "Lang":350, "Chart":5250, "Mockito":1176, "unknown":2800}
 args = dotdict({
     'NlLen':NlLen_map[sys.argv[2]],
     'CodeLen':CodeLen_map[sys.argv[2]],
